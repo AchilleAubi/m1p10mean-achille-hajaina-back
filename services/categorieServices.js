d6a916e39ac69fa808342546e8f59b6e632f9d58
Achille Aubin <achilleaubinfanomezantsoa@gmail.com>
2024-02-22 22:32:32 +0100

    change logique in employe

--- services/categorieServices.js
+++ services/categorieServices.js
@@ -2,6 +2,17 @@ const Categorie = require('../models/categorie');
 const ServiceSalonServices = require('../services/serviceSalonServices');
 
 const categorieServices = {
+
+    async getAllCategorie() {
+        try {
+            const categories = await Categorie.find({});
+            return categories;
+        } catch (error) {
+            console.log(error);
+            throw new error(error.message);
+        }
+    },
+
     async getCategorie() {
         try {
             const formattedCategories = {
