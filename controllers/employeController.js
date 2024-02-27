cc578840b557354d145dc365ccb46c0fe5be55be
Andriantsitohaina Hajaina <74816899+mamyhajaina@users.noreply.github.com>
2024-02-27 03:40:21 +0300

    [Commision]

--- controllers/employeController.js
+++ controllers/employeController.js
@@ -1,6 +1,10 @@
 const EmployeServices = require('../services/employeServices');
 const CategorieServices = require('../services/categorieServices');
+const RendezVousServices = require('../services/rendezVousServices');
 const asyncHandler = require('express-async-handler');
+const moment = require('moment');
+
+let invalidToken = [];
 
 const portFeuilleController = {
 
@@ -30,6 +34,44 @@ const portFeuilleController = {
             result.content = data;
             result.status = true;
             res.status(200).json(data);
+        } catch (error) {
+            res.status(500);
+            throw new error(error.message);
+        }
+    }),
+
+    commissionJouree: asyncHandler(async (req, res) => {
+        try {
+            const token = req.headers.authorization.split(' ')[1];
+            invalidToken.push(token);
+            const result = { totalServiceEffectuer: 0, commission: 0, totalPourcentage: 0, status: true };
+            const dateNow = moment(req.body.dateNow, "YYYY-MM-DD").toDate();
+            const idEmploye = req.body.idEmploye;
+            const rendeVous = await RendezVousServices.getRendezVousTermineByEmploye(idEmploye);
+            let totalPourcentage = 0;
+            let date = '';
+            let conte = 0;
+            let montant = 0;
+            console.log('totalPourcentage', totalPourcentage);
+            for (let item of rendeVous) {
+                for (let etat of item.etat) {
+                    if (etat.name == 'Terminer') {
+                        date = moment(etat.date, "YYYY-MM-DD").toDate();
+                    }
+                }
+                if (dateNow <= date) {
+                    conte = conte + 1;
+                    montant = montant + item.Service.prix;
+                    totalPourcentage = totalPourcentage + item.Service.commision;
+                }
+            }
+            console.log('conte', conte);
+            // totalPourcentage = conte * 5;
+            result.totalServiceEffectuer = conte;
+            result.commission = montant;
+            result.totalPourcentage = totalPourcentage;
+            result.status = true;
+            res.status(200).json(result);
         } catch (error) {
             res.status(500);
             throw new error(error.message);
